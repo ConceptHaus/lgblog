16f75996870081da514946ad64435eb54d8fcf2c
LeslyePO <79282745+LeslyePO@users.noreply.github.com>
2022-09-27 00:13:08 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'db_lgblog' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'forge' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'ipJlBz5ays4ho80oULXY' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
